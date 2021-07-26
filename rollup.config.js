a19a76bbc637b97f4d61878806c53203144cc6c8
Eduardo San Martin Morote <posva13@gmail.com>
2021-07-26 16:58:59 +0200

    build: externalize vue-demi

--- rollup.config.js
+++ rollup.config.js
@@ -77,7 +77,7 @@ function createConfig(format, output, plugins = []) {
   output.sourcemap = !!process.env.SOURCE_MAP
   output.banner = banner
   output.externalLiveBindings = false
-  output.globals = { vue: 'Vue' }
+  output.globals = { 'vue-demi': 'VueDemi' }
 
   const isProductionBuild = /\.prod\.js$/.test(output.file)
   const isGlobalBuild = format === 'global'
@@ -107,7 +107,7 @@ function createConfig(format, output, plugins = []) {
   // during a single build.
   hasTSChecked = true
 
-  const external = ['vue']
+  const external = ['vue-demi']
 
   const nodePlugins = [resolve(), commonjs()]
 
