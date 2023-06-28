986e74624337ae3857bd4e08995354dcadb2b4c4
Eduardo San Martin Morote <posva13@gmail.com>
2023-06-28 11:23:32 +0200

    test: use vitest

--- __tests__/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-export default {
-  env: {
-    jest: true,
-  },
-}
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,6 @@
 // import { useSpring } from '../src'
 // import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
 
 describe('useSpring', () => {
   it('works', () => {
